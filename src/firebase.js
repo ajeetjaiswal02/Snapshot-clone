a17d85822a3cf70f05b8a1d0e1590542e43a31ea
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2021-01-30 16:06:57 +0530

    Firebaseconfig done

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJKTZ2Tl2JyOpcO7gbrRfM52zpwh5q4q0",
+    authDomain: "snapshot-clone-4913c.firebaseapp.com",
+    projectId: "snapshot-clone-4913c",
+    storageBucket: "snapshot-clone-4913c.appspot.com",
+    messagingSenderId: "875611235722",
+    appId: "1:875611235722:web:661bad2ba5c7fb19215473"
+};
+
+
+const firebaseapp = firebase.intializeApp(firebaseConfig);
