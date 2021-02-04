7a0b106e3437447e8ba12a767edcb607076050c5
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2021-02-04 23:03:47 +0530

    add some new files

--- src/Chats.js
+++ src/Chats.js
@@ -3,8 +3,25 @@ import './Chats.css';
 import { Avatar } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import { db } from './firebase';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-function chats() {
+function Chats() {
+
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        db.collection("posts")
+          .orderBy('timestamp', 'desc')
+          .onSnapshot((snapshot) => 
+          setPosts(
+              snapshot.docs.map((doc) => ({
+                  id: doc.id,
+                  data: doc.data()
+              }))
+          ))
+    },[]);
+
     return (
         <div className="chats">
             <div className="chats_header">
@@ -15,11 +32,26 @@ function chats() {
                 </div>
                 <ChatBubbleIcon className="chats_chaticon" /> 
             </div>
-            <div className="chat_posts">
-                
+            <div className="chats_posts">
+                {posts.map(
+                    ({
+                        id,
+                        data: { profilePic, username, timestamp, imageUrl, read },
+                    }) => (
+                        <Chat
+                           key={id}
+                           id = {id}
+                           username = {username}
+                           timestamp = {timestamp}      
+                           imageUrl={imageUrl}
+                           read = {read}
+                           profilePic={profilePic}
+                        />
+                    )
+                )}
             </div>
         </div>
     )
 }
 // 15526 v
-export default chats
+export default Chats
